b5b4f03b9dbbd363f84fb34fae3f573e6dd2e89a
Funcamilo12 <camilobv28@gmail.com>
2024-11-27 09:42:57 -0500

    cambios

--- venta.js
+++ venta.js
@@ -1,6 +1,31 @@
-const btnCart = document.querySelector('.icon-cart')
+const btnCart = document.querySelector('.container-cart-icon')
 
 const containerCartProducts = document.querySelector('.container-carts-products')
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart')
+})
+
+/* """"""""""" */
+
+const CartInfo= document.querySelector('.cart-product')
+const rowProduct = document.querySelector('.row-product')
+
+//Lista de todos los contenedores de productos
+
+const productList=document.querySelector('.container-items')
+
+//Variable arreglo de productos
+
+let allProducts=[]
+
+
+
+
+
+productList.addEventListener('click',(e)=>{
+
+    if(e.target.classList.contains('btn-add-cart')){
+        
+    }
+    
 })

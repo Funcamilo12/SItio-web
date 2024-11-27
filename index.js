34778c5cd998ea890d9a222737ff8450b56d0247
Funcamilo12 <camilobv28@gmail.com>
2024-11-26 20:41:24 -0500

    load

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-count 
+const btnCart = document.querySelector('.icon-cart')
+
+const containerCartProducts = document.querySelector('.container-carts-products')
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
